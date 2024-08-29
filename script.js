e4199f2820170a2ff90df835f8e56bd6dfb30e74
anto <pepito@gmail.com>
2024-08-29 20:34:12 -0300

    modificamos una card

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 console.log ("no se que hacer ayuda");
 console.log ("otra vez estoy haciendo pruebas");
+
+
+
+const H5 = document.querySelector("h5");
+H5.innerText = "dibu";
+
+const text = document.querySelector(".card-text");
+text.innerText = "dibu";
+
+const img = document.querySelector(".card img");
+img.src = "https://i.pinimg.com/564x/01/df/c4/01dfc493a5a64a3a6c0b050cbdfa5840.jpg";
